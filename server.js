c5c3a22b0e2cfabe96c0c206a139225f3abbd863
Alena Provotorova <lunichkina.alena@gmail.com>
2022-06-15 16:15:20 +0300

    add upload files and express

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 require('dotenv').config()
-import { ApolloServer, gql } from 'apollo-server'
-import schema from './schema';
+import express from 'express'
+import { ApolloServer } from 'apollo-server-express'
+import logger from 'morgan'
+import {typeDefs, resolvers} from './schema';
 import { getUser, protectResolvers } from './users/users.utils';
 
-  const server = new ApolloServer({schema, context: async({req}) => {
+  const apollo = new ApolloServer({typeDefs, resolvers, context: async({req}) => {
     return  {
       loggedInUser: await getUser(req.headers.token),
       protectResolvers
@@ -11,9 +13,17 @@ import { getUser, protectResolvers } from './users/users.utils';
   }});
 
   const PORT = process.env.PORT
+
+  const app = express()
+  app.use(logger('tiny'))
+  app.use('/static', express.static("uploads"))
+
+  apollo.applyMiddleware({app})
   
-  server
-  .listen(PORT)
-  .then(() => {
-    console.log(`🚀 Server ready at http://localhost:${PORT}`);
-  });
+  app.listen({port: PORT}, () => {
+      console.log(`🚀 Server ready at http://localhost:${PORT}`);
+    })
+ 
+
+  //todo
+  // вернуть файл к предыдущему состоянию, если будет использоваться облачное хранилище
--- users/editProfile/editProfile.resolvers.js
+++ users/editProfile/editProfile.resolvers.js
@@ -1,21 +1,35 @@
 import client from "../../client"
 import bcrypt from 'bcrypt'
 import { protectedResolvers } from "../users.utils"
+import {createWriteStream} from 'fs'
 
 export default {
     Mutation: {
         editProfile: protectedResolvers(async (
             _, 
-            {firstName, lastName, userName, email, password: newPassword},
+            {firstName, lastName, userName, email, bio, avatar, password: newPassword },
             {loggedInUser}
             ) => {
+                let avatarUrl = null
+        if(avatar) {
+        // TODO use cloud store for files
+        const {filename, createReadStream} = await avatar
+        const readStream = createReadStream()
+        const newFileName = `${loggedInUser.id}-${Date.now()}-${filename}`
+        const writeStream = createWriteStream(process.cwd() + "/uploads/" + newFileName)
+        readStream.pipe(writeStream)
+        avatarUrl = `http://localhost:4000/static/${newFileName}`
+        }
+       
         let hashedPassword = null
         if(newPassword) {
             hashedPassword = await bcrypt.hash(newPassword, 10)
         }
         const editedUser = await client.user.update({
             where: {id: loggedInUser.id}, 
-            data: {firstName, lastName, userName, email, ...(hashedPassword && {password: hashedPassword})}
+            data: {firstName, lastName, userName, email, bio,
+                ...(avatarUrl && {avatar: avatarUrl}),
+                 ...(hashedPassword && {password: hashedPassword})}
         })
 
         if(editedUser.id) {
